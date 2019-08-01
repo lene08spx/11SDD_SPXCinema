843e968aa7e17e7bb09dae084d07236332a53f18
Oliver L <lene08spx@gmail.com>
2019-08-01 12:08:34 +1000

    basic level stuffs

--- src/deps.ts
+++ src/deps.ts
@@ -1,14 +1,12 @@
 import * as DenoHttp from "../lib/deno_std/http/server.ts";
 import * as DenoFs from "../lib/deno_std/fs/mod.ts";
 import * as DenoPath from "../lib/deno_std/fs/path/mod.ts";
-import * as DenoColour from "../lib/deno_std/colors/mod.ts";
 import * as DenoMediaTypes from "../lib/deno_std/media_types/mod.ts";
 
 export
 {
-    DenoHttp,
-    DenoFs,
-    DenoPath,
-    DenoColour,
-    DenoMediaTypes,
+	DenoHttp,
+	DenoFs,
+	DenoPath,
+	DenoMediaTypes,
 };
--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,51 @@
-import { DenoHttp, DenoFs, DenoPath } from "./deps.ts";
+import { DenoHttp, DenoFs, DenoPath, DenoMediaTypes } from "./deps.ts";
 
 const WEB_PORT = 80;
 const WEB_ROOT = "./src/web"
 
 async function servePage( r: DenoHttp.ServerRequest, filename?: string ): Promise<void>
 {
-    let filePath = WEB_ROOT + (filename || r.url);
-    console.log(filePath);
-    if (await DenoFs.exists( filePath ))
-    {
-        r.respond({
-            "body": await Deno.readFile( filePath ),
-            "status": 200
-        });
-    }
-    else
-    {
-        r.respond({
-            "body": new TextEncoder().encode("<!DOCTYPE html><h1>404</h1>"),
-            "status": 404
-        });
-    }
+	let filePath = WEB_ROOT + (filename || r.url);
+	//console.log(filePath);
+	if (await DenoFs.exists( filePath ))
+	{
+		// Serve Page
+		console.log(`?> ${filePath}`);
+		r.respond({
+			"body": await Deno.readFile( filePath ),
+			"headers": new Headers([
+				["Content-Type",DenoMediaTypes.contentType(filePath)||"text/plain"]
+			]),
+			"status": 200
+		});
+	}
+	else
+	{
+		// Error 404
+		console.log(`!> 404`);
+		r.respond({
+			"body": new TextEncoder().encode("<!DOCTYPE html><h1>404</h1>"),
+			"headers": new Headers([
+				["Content-Type","text/plain"]
+			]),
+			"status": 404
+		});
+	}
 }
-
-async function main(): Promise<void>
+window.onload = async()=>
 {
-    const s = DenoHttp.serve(`0.0.0.0:${WEB_PORT}`);
-    console.log(`SPX Cinema @ http://localhost:${WEB_PORT}/`);
-    for await (const r of s) {
-        switch (r.url) {
-            case "/":
-                servePage( r, "/index.html" );
-                break;
-            default:
-                servePage( r );
-                break;
-        }
-    }
-    return;
+	const s = DenoHttp.serve(`0.0.0.0:${WEB_PORT}`);
+	console.log("-> Finished Compilation");
+	console.log(`-> SPXCinema@http://localhost:${WEB_PORT}/`);
+	for await (const r of s) {
+		switch (r.url) {
+			case "/":
+				servePage( r, "/index.html" );
+				break;
+			default:
+				servePage( r );
+				break;
+		}
+	}
+	return;
 }
-main();
